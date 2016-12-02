b0639748d8085c00ec6cad847821c252343ece03
Nick Gerner <hello@njgerner.com>
2016-12-02 14:51:54 -0500

    all tests pass

--- test/test-GhostSequelize.js
+++ test/test-GhostSequelize.js
@@ -1,26 +1,62 @@
+const Config = require('config');
 const Chai = require('chai');
 const expect = Chai.expect;
 const _ = require('lodash');
 
 const GhostSequelize = require('../index');
-const ghostSequelize = new GhostSequelize(Config.get('database'));
+const Db = new GhostSequelize(Config.get('database'), __dirname);
 
 describe('GhostSequelize', () => {
 
-  beforeEach(() => ghostSequelize.sync());
+  beforeEach(() => Db.sync());
 
   describe('dropTable', () => {
 
     it('should drop a table', () => {
-
+      return Db.dropTable('test')
     })
 
   });
 
-  describe('getDb', () => {});
-  describe('sync', () => {});
-  describe('syncTable', () => {});
-  describe('truncateTable', () => {});
-  describe('truncateTables', () => {});
+  describe('getDb', () => {
+
+    it('should get the db', () => {
+      return Db.getDb();
+    })
+
+  });
+
+  describe('sync', () => {
+
+    it('should sync the db', () => {
+      return Db.sync();
+    })
+
+  });
+
+
+  describe('syncTable', () => {
+
+    it('should sync a table', () => {
+      return Db.syncTable('test');
+    })
+
+  });
+
+  describe('truncateTable', () => {
+
+    it('should truncate a table', () => {
+      return Db.truncateTable('test');
+    })
+
+  });
+
+  describe('truncateTables', () => {
+
+    it('should truncate tables', () => {
+      return Db.truncateTable(['test']);
+    })
+
+  });
 
 });
